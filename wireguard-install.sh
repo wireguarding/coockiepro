72e26bc73fa6703597153883296c9dc77d70f921
wireguarding <148226296+wireguarding@users.noreply.github.com>
2023-10-17 16:28:03 +0300

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -1,49 +1,45 @@
 #!/bin/bash
 
-# Устанавливаем необходимые пакеты
+# Установка WireGuard
 apt-get update
-apt-get install -y wireguard qrencode
+apt-get install -y wireguard
 
-# Генерируем ключи
+# Генерация ключей
 umask 077
-wg genkey > server_private_key
-wg genkey > client_private_key
-wg pubkey < server_private_key > server_public_key
-wg pubkey < client_private_key > client_public_key
+wg genkey | tee server_private_key | wg pubkey > server_public_key
+wg genkey | tee client_private_key | wg pubkey > client_public_key
 
-SERVER_PRIVATE_KEY=$(cat server_private_key)
-SERVER_PUBLIC_KEY=$(cat server_public_key)
-CLIENT_PRIVATE_KEY=$(cat client_private_key)
-CLIENT_PUBLIC_KEY=$(cat client_public_key)
+server_private_key=$(cat server_private_key)
+server_public_key=$(cat server_public_key)
+client_private_key=$(cat client_private_key)
+client_public_key=$(cat client_public_key)
 
-# Автоматически определяем публичный IP-адрес сервера
-SERVER_IP=$(curl -s https://ifconfig.me)
+# Получение публичного IP-адреса сервера
+server_ip=$(curl -s ifconfig.me)
 
-# Создаем конфигурационный файл сервера
+# Создание конфигурационного файла сервера
 cat <<EOL > /etc/wireguard/wg0.conf
 [Interface]
 Address = 10.0.0.1/24
-PrivateKey = ${SERVER_PRIVATE_KEY}
 ListenPort = 51820
+PrivateKey = $server_private_key
 
 [Peer]
-PublicKey = ${CLIENT_PUBLIC_KEY}
+PublicKey = $client_public_key
 AllowedIPs = 10.0.0.2/32
 EOL
 
-# Запускаем WireGuard
+# Запуск WireGuard сервера
 wg-quick up wg0
-systemctl enable wg-quick@wg0
 
-# Выводим конфигурационный файл для клиента
-echo "Конфигурация клиента:"
+# Создание конфигурационного файла клиента
 cat <<EOL
 [Interface]
 Address = 10.0.0.2/24
-PrivateKey = ${CLIENT_PRIVATE_KEY}
+PrivateKey = $client_private_key
 
 [Peer]
-PublicKey = ${SERVER_PUBLIC_KEY}
-Endpoint = ${SERVER_IP}:51820
+PublicKey = $server_public_key
+Endpoint = $server_ip:51820
 AllowedIPs = 0.0.0.0/0
 EOL
