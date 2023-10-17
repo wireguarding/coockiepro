25ccac246f9dcd604d9f6e9ec5aaac97fa5aaee5
wireguarding <148226296+wireguarding@users.noreply.github.com>
2023-10-17 16:11:14 +0300

    Update wireguard-install.sh

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
-# Обновление и установка необходимых пакетов
-sudo apt update && sudo apt install -y wireguard
+# Устанавливаем необходимые пакеты
+apt-get update
+apt-get install -y wireguard qrencode
 
-# Генерация серверных ключей
-wg genkey | tee server_private_key | wg pubkey > server_public_key
+# Генерируем ключи
+umask 077
+wg genkey > server_private_key
+wg genkey > client_private_key
+wg pubkey < server_private_key > server_public_key
+wg pubkey < client_private_key > client_public_key
 
-# Настройка конфига сервера
-echo "[Interface]
+SERVER_PRIVATE_KEY=$(cat server_private_key)
+SERVER_PUBLIC_KEY=$(cat server_public_key)
+CLIENT_PRIVATE_KEY=$(cat client_private_key)
+CLIENT_PUBLIC_KEY=$(cat client_public_key)
+
+# Автоматически определяем публичный IP-адрес сервера
+SERVER_IP=$(curl -s https://ifconfig.me)
+
+# Создаем конфигурационный файл сервера
+cat <<EOL > /etc/wireguard/wg0.conf
+[Interface]
 Address = 10.0.0.1/24
-SaveConfig = true
-PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+PrivateKey = ${SERVER_PRIVATE_KEY}
 ListenPort = 51820
-PrivateKey = $(cat server_private_key)
-
-" > /etc/wireguard/wg0.conf
-
-# Генерация клиентских ключей
-wg genkey | tee client_private_key | wg pubkey > client_public_key
-
-# Настройка конфига клиента
-echo "[Interface]
-Address = 10.0.0.2/24
-PrivateKey = $(cat client_private_key)
 
 [Peer]
-PublicKey = $(cat server_public_key)
-Endpoint = <your_server_ip>:51820
-AllowedIPs = 0.0.0.0/0
+PublicKey = ${CLIENT_PUBLIC_KEY}
+AllowedIPs = 10.0.0.2/32
+EOL
 
-" > client.conf
+# Запускаем WireGuard
+wg-quick up wg0
+systemctl enable wg-quick@wg0
 
-# Запуск сервера
-sudo wg-quick up wg0
-sudo systemctl enable wg-quick@wg0
-
+# Выводим конфигурационный файл для клиента
 echo "Конфигурация клиента:"
-cat client.conf
+cat <<EOL
+[Interface]
+Address = 10.0.0.2/24
+PrivateKey = ${CLIENT_PRIVATE_KEY}
+
+[Peer]
+PublicKey = ${SERVER_PUBLIC_KEY}
+Endpoint = ${SERVER_IP}:51820
+AllowedIPs = 0.0.0.0/0
+EOL
